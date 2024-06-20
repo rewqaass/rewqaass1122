aaf5f8f90dfe9c457f01010f29c204234881ea11
rewqaass <147226966+rewqaass@users.noreply.github.com>
2024-06-20 13:06:14 +0900

    Add files via upload

---
+++ 0607/0608.js
@@ -0,0 +1,13 @@
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+const slider = document.querySelector('.slider')
+
+next.addEventListener('click', function(){
+	const slides = document.querySelectorAll('.slides')
+  slider.appendChild(slides[0])
+})
+
+prev.addEventListener('click', function(){
+	const slides = document.querySelectorAll('.slides')
+  slider.prepend(slides[slides.length-1]);
+})
